fbb78669dd5f3f2b988d2ca48d6e069cb323d17a
SebUndefined <me@sebundefined.fr>
2017-03-09 17:17:45 +0100

    Adding contact layout and route

--- app/routes.php
+++ app/routes.php
@@ -2,3 +2,7 @@
 
 $app->get('/', "BlogWriter\Controller\HomeController::indexAction")
 ->bind('home');
+
+
+$app->get('/contact', "BlogWriter\Controller\HomeController::contactAction")
+->bind('contact');
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,6 +20,16 @@ class HomeController
 		return $app['twig']->render('index.html.twig');
 	}
 	
+	/**
+	 * Contact page controller.
+	 *
+	 * @param Application $app Silex application retourne les éléments pertinents pour la vue
+	 */
+	public function contactAction(Application $app) {
+		// 		$articles = $app['dao.article']->findAll();
+		// 		return $app['twig']->render('index.html.twig', array('articles' => $articles));
+		return $app['twig']->render('contact.html.twig');
+	}
 	
 	
 }
